4becbab4b2dcd41883a8621dc80a0fd7110c28d5
Ryoya Chiba <ryokan.jap.83.c@gmail.com>
2024-02-12 17:34:32 +0900

    initial commit

---
+++ src/main/java/com/code/devtask/DevTaskApplication.java
@@ -0,0 +1,13 @@
+package com.code.devtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTaskApplication.class, args);
+	}
+
+}
