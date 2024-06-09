fcbf4b23b673dc12e7c47e591787cab210a8d826
Ryoya Chiba <ryokan.jap.83.c@gmail.com>
2024-06-09 13:47:59 +0900

    fix: delete unnecessary parameters

--- src/main/java/com/code/devtask/task/domain/Task.java
+++ src/main/java/com/code/devtask/task/domain/Task.java
@@ -14,11 +14,9 @@ public class Task {
     private UUID id;
     private String title;
     private String description;
-    private LocalDate createdAt;
-    private LocalDate updatedAt;
 
     /*
-     * MyBatis用のコンストラクタ
+     * コンストラクタ
      * 
      * @param title タスクのタイトル
      * 
@@ -28,26 +26,6 @@ public class Task {
         this.id = UUID.randomUUID();
         this.title = title;
         this.description = description;
-        this.createdAt = LocalDate.now();
-        this.updatedAt = LocalDate.now();
     }
 
-    /*
-     * MyBatis用のコンストラクタ
-     * 
-     * @param title タスクのタイトル
-     * 
-     * @param description タスクの説明
-     * 
-     * @param createdAt タスクの作成日
-     * 
-     * @param updatedAt たすくの更新日
-     */
-    public Task(String title, String description, LocalDate createdAt, LocalDate updatedAt) {
-        this.id = UUID.randomUUID();
-        this.title = title;
-        this.description = description;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
 }
